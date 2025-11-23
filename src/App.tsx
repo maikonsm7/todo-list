777bbf87b9da1fb26f20534e7a14148085db3e2b
maikonsm7 <maikonsm7@gmail.com>
2025-11-23 17:03:04 -0400

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,36 @@ import './App.css'
 import { Task } from './components/Task'
 
 function App() {
-  const [tasks, setTasks] = useState<string[]>(JSON.parse(localStorage.getItem('tasks') || '[]'))
+  type Task = {
+    title: string,
+    completed: boolean
+  }
+  
+  const [tasks, setTasks] = useState<Task[]>(JSON.parse(localStorage.getItem('tasks') || '[{}]'))
   const [task, setTask] = useState('')
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
-    setTasks([...tasks, task])
-    localStorage.setItem('tasks', JSON.stringify([...tasks, task]))
+    setTasks([...tasks, { title: task, completed: false }])
+    localStorage.setItem('tasks', JSON.stringify([...tasks, { title: task, completed: false }]))
     setTask('')
+  }
+
+  const removeTask = (index: number) => {
+    const newTasks = tasks.filter((_, i) => i !== index)
+    setTasks(newTasks)
+    localStorage.setItem('tasks', JSON.stringify(newTasks))
+  }
+
+  const taskCompleted = (index: number) => {
+    const newTasks = tasks.map((task, i) => {
+      if (i === index) {
+        return { ...task, completed: !task.completed }
+      }
+      return task
+    })
+    setTasks(newTasks)
+    localStorage.setItem('tasks', JSON.stringify(newTasks))
   }
 
   return (
@@ -22,7 +44,7 @@ function App() {
       </form>
 
         {tasks.map((t, index) => (
-          <Task key={index} task={t} />
+          <Task key={index} task={t} removeTask={() => removeTask(index)} taskCompleted={() => taskCompleted(index)} />
         ))}
 
     </>
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,11 +1,15 @@
 import styles from './Task.module.css'
-export const Task = ({ task }: { task: string }) => {
+export const Task = (
+    { task, removeTask, taskCompleted } : { task: { title: string, completed: boolean },
+    removeTask: () => void,
+    taskCompleted: () => void
+}) => {
   return (
     <div className={styles['card-task']}>
-        <span>{task}</span>
+        <span className={task.completed ? styles.through : ''}>{task.title}</span>
         <div className={styles.actions}>
-            <button className={styles.btnActions}>View</button>
-            <button className={styles.btnActions}>Delete</button>
+            <button className={styles.btnActions} onClick={() => taskCompleted()}>{task.completed ? 'Completed' : 'Incomplete'}</button>
+            <button className={styles.btnActions} onClick={() => removeTask()}>Delete</button>
         </div>
     </div>
   )
